6d3decd3ef44c1af7ff584b6a98b3399347bf5d2
gventurini <giacomo.venturini3@gmail.com>
2021-10-03 18:04:38 +0200

    fix style

--- src/index.mjs
+++ src/index.mjs
@@ -23,12 +23,12 @@ try {
     shelljs.config.verbose = true;
 
     const currentBranch = shelljs.exec(`git branch --show-current`);
-    const targetBranch = argv.b;
-    const mergeStrategy = argv.c;
+    const targetBranch = argv['b'];
+    const mergeStrategy = argv['c'];
 
     // necessary conversion: shell js stdout contains a new line
     if (currentBranch.stdout.replace('\n', '') === targetBranch) {
-        console.warn(`Nothing to do: current branch and target branch are the same!`);
+        console.warn('Nothing to do: current branch and target branch are the same!');
         process.exit(1);
     }
 
@@ -36,7 +36,7 @@ try {
     shelljs.exec(`git pull`);
     shelljs.exec(`git checkout ${currentBranch}`);
     shelljs.exec(`git ${mergeStrategy} ${targetBranch}`);
-} catch (p) {
-    console.error(`Exit code: ${p.exitCode}`)
-    console.error(`Error: ${p.stderr}`)
+} catch (e) {
+    console.error(`Exit code: ${e.exitCode}`);
+    console.error(`Error: ${e.stderr}`);
 }
